7ba18fef05ad9de67d1156fddcb04e1b17cc662c
Linkaform Develop Team <backend@linkaform.com>
2024-03-29 21:54:39 -0600

    se separa warehouse en nueva clase

--- lkf_addons/addons/product/product_utils.py
+++ lkf_addons/addons/product/product_utils.py
@@ -15,25 +15,6 @@ class Product(base.LKF_Base):
         self.PRODUCT_ID = self.PRODUCT.get('id')
         self.PRODUCT_OBJ_ID = self.PRODUCT.get('obj_id')
 
-
-
-        self.WAREHOUSE = self.lkm.catalog_id('warehouse')
-        self.WAREHOUSE_ID = self.WAREHOUSE.get('id')
-        self.WAREHOUSE_OBJ_ID = self.WAREHOUSE.get('obj_id')
-
-        self.WAREHOUSE_LOCATION = self.lkm.catalog_id('warehouse_locations')
-        self.WAREHOUSE_LOCATION_ID = self.WAREHOUSE_LOCATION.get('id')
-        self.WAREHOUSE_LOCATION_OBJ_ID = self.WAREHOUSE_LOCATION.get('obj_id')
-
-        self.WAREHOUSE_DEST = self.lkm.catalog_id('warehouse_destination')
-        self.WAREHOUSE_DEST_ID = self.WAREHOUSE_LOCATION.get('id')
-        self.WAREHOUSE_DEST_OBJ_ID = self.WAREHOUSE_LOCATION.get('obj_id')
-
-
-        self.WAREHOUSE_LOCATION_DEST = self.lkm.catalog_id('warehouse_location_destination')
-        self.WAREHOUSE_LOCATION_DEST_ID = self.WAREHOUSE_LOCATION_DEST.get('id')
-        self.WAREHOUSE_LOCATION_DEST_OBJ_ID = self.WAREHOUSE_LOCATION_DEST.get('obj_id')
-
         try:
             self.SKU = self.lkm.catalog_id('sku_catalog')
             self.SKU_ID = self.SKU.get('id')
@@ -68,6 +49,40 @@ class Product(base.LKF_Base):
             'sku_percontainer':'6205f73281bb36a6f157335b',
             'sku_size':'6205f73281bb36a6f1573358',
             'sku_source':'6205f73281bb36a6f157335a',
+            }
+            )
+
+    def una_funcion_product(self):
+        return True
+
+class Warehouse(base.LKF_Base):
+
+
+    def __init__(self, settings, folio_solicitud=None, sys_argv=None, use_api=False):
+        # base.LKF_Base.__init__(self, settings, sys_argv=sys_argv)
+        super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
+        self.name =  __class__.__name__
+        self.settings = settings
+
+        self.WAREHOUSE = self.lkm.catalog_id('warehouse')
+        self.WAREHOUSE_ID = self.WAREHOUSE.get('id')
+        self.WAREHOUSE_OBJ_ID = self.WAREHOUSE.get('obj_id')
+
+        self.WAREHOUSE_LOCATION = self.lkm.catalog_id('warehouse_locations')
+        self.WAREHOUSE_LOCATION_ID = self.WAREHOUSE_LOCATION.get('id')
+        self.WAREHOUSE_LOCATION_OBJ_ID = self.WAREHOUSE_LOCATION.get('obj_id')
+
+        self.WAREHOUSE_DEST = self.lkm.catalog_id('warehouse_destination')
+        self.WAREHOUSE_DEST_ID = self.WAREHOUSE_LOCATION.get('id')
+        self.WAREHOUSE_DEST_OBJ_ID = self.WAREHOUSE_LOCATION.get('obj_id')
+
+
+        self.WAREHOUSE_LOCATION_DEST = self.lkm.catalog_id('warehouse_location_destination')
+        self.WAREHOUSE_LOCATION_DEST_ID = self.WAREHOUSE_LOCATION_DEST.get('id')
+        self.WAREHOUSE_LOCATION_DEST_OBJ_ID = self.WAREHOUSE_LOCATION_DEST.get('obj_id')
+
+
+        self.f.update( {
             'warehouse':'6442e4831198daf81456f274',
             'warehouse_dest':'65bdc71b3e183f49761a33b9',
             'warehouse_location':'65ac6fbc070b93e656bd7fbe',
@@ -75,7 +90,27 @@ class Product(base.LKF_Base):
             'warehouse_type':'6514f51b6cfe23860299abfa',
             'warehouse_type_dest':'65bdc74a9c6a5b1adf424b5b',
             }
-            )
+        )
 
-    def una_funcion_product(self):
-        return True
+    def get_all_stock_warehouse(self):
+        return self.get_warehouse(warehouse_type='Stock')
+
+    def get_warehouse(self, warehouse_type=None):
+        mango_query = {
+            "selector":{"_id": {"$gt":None}},
+            "limit":1000,
+            "skip":0
+            }
+        if warehouse_type:
+            mango_query['selector'] = {'answers':{self.f['warehouse_type']: warehouse_type}}
+        res = self.lkf_api.search_catalog( self.CATALOG_WAREHOUSE_ID, mango_query)
+        warehouse = [r[self.f['warehouse']] for r in res]
+        return warehouse
+
+    def warehouse_type(self, warehouse_name):
+        answers = {f"{self.f['warehouse']}":warehouse_name}
+        catalog_record = self.lkf_api.search_catalog_answers(self.WAREHOUSE_ID, answers,  jwt_settings_key='APIKEY_JWT_KEY',**{'limit':1})
+        if not catalog_record:
+            self.LKFException(f"Warehouse: {warehouse_name}, not found or dont have the correct access, please check with you admin")
+        wh_type = catalog_record.get(f"{self.f['warehouse_type']}")
+        return wh_type
