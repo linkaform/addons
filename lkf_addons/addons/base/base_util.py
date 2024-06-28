18462638b4cfa0b36098531c78bbe1fb535d5d81
KarinaMoreno <dianakarina.moreno21@gmail.com>
2024-06-28 11:46:16 -0600

    Agregué documentación en el archivo base_utils.py del módulo base

--- lkf_addons/addons/base/base_util.py
+++ lkf_addons/addons/base/base_util.py
@@ -1,16 +1,56 @@
 # -*- coding: utf-8 -*-
+### Linkaform Modules / Archivo de Módulo ###
+'''
+Este archivo proporciona las funcionalidades modulares de LinkaForm. Con estas funcionalidades, 
+podrás utilizar la plataforma LinkaForm de manera modular, como un Backend as a Service (BaaS).
 
-from linkaform_api import base
+Licencia
+Este código está licenciado bajo la licencia GPL3 (https://www.gnu.org/licenses/gpl-3.0.html).
+
+Propósito
+El propósito de este archivo es ser auto documentable y adaptable, facilitando la reutilización 
+de gran parte del código en otros módulos simplemente copiando y pegando las secciones necesarias.
+
+Instrucciones
+1. Al copiar secciones de código, asegúrate de incluir la documentación correspondiente.
+2. Al crear un nuevo archivo o módulo, copia las instrucciones y las generales aplicables a cada archivo.
+3. Puedes basarte en la carpeta `_templates` o sus archivos para crear nuevos módulos.
+'''
+
+### Archivo de Modulo ###
+'''
+Este archivo define las funciones generales del módulo. Por conveniencia, se nombra `app.py`. 
+
+Si tienes más de una aplicación, puedes:
+    a. Crear una carpeta llamada `app`.
+    b. Guardar los archivos a nivel raíz.
+    c. Nombrar los archivos por conveniencia o estándar: `app_utils.py`, `utils.py`, `xxx_utils.py`.
+'''
+
+# Importaciones necesarias
+from linkaform_api.base import LKF_Base
 
 
 class Base(base.LKF_Base):
 
     def __init__(self, settings, folio_solicitud=None, sys_argv=None, use_api=False):
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
-        #use self.lkm.catalog_id() to get catalog id
-        # forms
+        #--Variables 
+        ### Forms ###
+        '''
+        `self.FORM_NAME = self.lkm.form_id('form_name',id)` ---> Aquí deberás guardar los `ID` de los formularios. 
+        Para ello deberás llamar el método `lkm.form_id` del objeto `lkm` (linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos.
+        '''
         self.CONTACTO = self.lkm.form_id('contacto', 'id')
-        # catalgos
+
+        #--Variables 
+        ### Catálogos ###
+        '''
+        `self.CATALOG_NAME = self.lkm.catalog_id('catalog_name',id)` ---> Aquí deberás guardar los `ID` de los catálogos. 
+        Para ello deberás llamar el método `lkm.catalog_id` del objeto `lkm`(linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos).
+        '''
         self.COMPANY = self.lkm.catalog_id('compaia')
         self.COMPANY_ID = self.COMPANY.get('id')
         self.COMPANY_OBJ_ID = self.COMPANY.get('obj_id')
@@ -18,7 +58,6 @@ class Base(base.LKF_Base):
         self.CONTACTO_CAT = self.lkm.catalog_id('contacto')
         self.CONTACTO_CAT_ID = self.CONTACTO_CAT.get('id')
         self.CONTACTO_CAT_OBJ_ID = self.CONTACTO_CAT.get('obj_id')
-
 
         self.ESTADO = self.lkm.catalog_id('estados')
         self.ESTADO_ID = self.ESTADO.get('id')
@@ -31,6 +70,21 @@ class Base(base.LKF_Base):
         self.TIMEZONE = self.lkm.catalog_id('timezone')
         self.TIMEZONE_ID = self.TIMEZONE.get('id')
         self.TIMEZONE_OBJ_ID = self.TIMEZONE.get('obj_id')
+
+        ### Fields ###
+        '''
+        `self.f`: En esta variable "fields", se almacenan todos los campos de todos los módulos heredados.
+        El orden de reemplazo se ve afectado por el orden en que se hereda cada módulo. El orden que se otorga, es considerando
+        que la variable se iguala en la base, y se va armando en tren de dependencias ej.
+
+            Class A:
+            Class B(A):
+            Class C(B):
+            Class D(C):
+
+            x_obj = D()
+            el orden de herencia será, primero carga A > B > C > D.
+        '''
 
         self.f.update( {
             'address_name':'663a7e0fe48382c5b1230901',
@@ -51,3 +105,14 @@ class Base(base.LKF_Base):
             'cat_timezone':f'{self.TIMEZONE_OBJ_ID}.665e4f90c4cf32cb52ebe15c',
         }
         )
+        
+    '''
+    funciones internas: son funciones que solo se pueden mandar llamar dentro de este archivo. Si se hereda la clase
+    esta función no puede ser invocada.
+
+    pep-0008:
+        _single_leading_underscore: 
+        weak “internal use” indicator. E.g. from M import * does not import objects whose names start with an underscore.
+    
+    def _NAME(self, ):
+    '''
