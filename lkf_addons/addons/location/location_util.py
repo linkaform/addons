087221df56eca8cc93c6834e942588a01025c738
KarinaMoreno <dianakarina.moreno21@gmail.com>
2024-06-28 11:55:08 -0600

    Agregué documentación en el archivo location_utils.py del módulo location

--- lkf_addons/addons/location/location_util.py
+++ lkf_addons/addons/location/location_util.py
@@ -1,18 +1,68 @@
 # -*- coding: utf-8 -*-
+### Linkaform Modules / Archivo de Módulo ###
+'''
+Este archivo proporciona las funcionalidades modulares de LinkaForm. Con estas funcionalidades, 
+podrás utilizar la plataforma LinkaForm de manera modular, como un Backend as a Service (BaaS).
 
+Licencia
+Este código está licenciado bajo la licencia GPL3 (https://www.gnu.org/licenses/gpl-3.0.html).
+
+Propósito
+El propósito de este archivo es ser auto documentable y adaptable, facilitando la reutilización 
+de gran parte del código en otros módulos simplemente copiando y pegando las secciones necesarias.
+
+Instrucciones
+1. Al copiar secciones de código, asegúrate de incluir la documentación correspondiente.
+2. Al crear un nuevo archivo o módulo, copia las instrucciones y las generales aplicables a cada archivo.
+3. Puedes basarte en la carpeta `_templates` o sus archivos para crear nuevos módulos.
+'''
+
+### Archivo de Modulo ###
+'''
+Este archivo define las funciones generales del módulo. Por conveniencia, se nombra `app.py`. 
+
+Si tienes más de una aplicación, puedes:
+    a. Crear una carpeta llamada `app`.
+    b. Guardar los archivos a nivel raíz.
+    c. Nombrar los archivos por conveniencia o estándar: `app_utils.py`, `utils.py`, `xxx_utils.py`.
+'''
+
+# Importaciones necesarias
 from linkaform_api import base
 from lkf_addons.addons.base.base_util import Base
 
+### Objeto o Clase de Módulo ###
+'''
+Cada módulo puede tener múltiples objetos, configurados en clases.
+Estos objetos deben heredar de `base.LKF_Base` y de cualquier módulo dependiente necesario.
+Al utilizar `super()` en el método `__init__()`, heredamos las variables de configuración de la clase.
+
+Además, se pueden heredar funciones de cualquier clase antecesora usando el método `super()`.
+'''
 
 class Location(Base, base.LKF_Base):
 
     def __init__(self, settings, folio_solicitud=None, sys_argv=None, use_api=False):
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
         #use self.lkm.catalog_id() to get catalog id
-        # forms
+
+        #--Variables 
+        ### Forms ###
+        '''
+        `self.FORM_NAME = self.lkm.form_id('form_name',id)` ---> Aquí deberás guardar los `ID` de los formularios. 
+        Para ello deberás llamar el método `lkm.form_id` del objeto `lkm` (linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos.
+        '''
         self.AREAS_DE_LAS_UBICACIONES = self.lkm.form_id('areas_de_las_ubicaciones', 'id')
         self.UBICACIONES = self.lkm.form_id('ubicaciones', 'id')
-        # catalgos
+
+        #--Variables 
+        ### Catálogos ###
+        '''
+        `self.CATALOG_NAME = self.lkm.catalog_id('catalog_name',id)` ---> Aquí deberás guardar los `ID` de los catálogos. 
+        Para ello deberás llamar el método `lkm.catalog_id` del objeto `lkm`(linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos).
+        '''
         self.UBICACIONES_CAT = self.lkm.catalog_id('ubicaciones')
         self.UBICACIONES_CAT_ID = self.UBICACIONES_CAT.get('id')
         self.UBICACIONES_CAT_OBJ_ID = self.UBICACIONES_CAT.get('obj_id')
@@ -29,12 +79,36 @@ class Location(Base, base.LKF_Base):
         self.TIPO_AREA_ID = self.TIPO_AREA.get('id')
         self.TIPO_AREA_OBJ_ID = self.TIPO_AREA.get('obj_id')
 
+        ### Fields ###
+        '''
+        `self.f`: En esta variable "fields", se almacenan todos los campos de todos los módulos heredados.
+        El orden de reemplazo se ve afectado por el orden en que se hereda cada módulo. El orden que se otorga, es considerando
+        que la variable se iguala en la base, y se va armando en tren de dependencias ej.
+
+            Class A:
+            Class B(A):
+            Class C(B):
+            Class D(C):
+
+            x_obj = D()
+            el orden de herencia será, primero carga A > B > C > D.
+        '''
+
         self.f.update( {
             'location':'663e5c57f5b8a7ce8211ed0b',
             'area':'663e5d44f5b8a7ce8211ed0f',
         }
         )
 
+    '''
+    funciones internas: son funciones que solo se pueden mandar llamar dentro de este archivo. Si se hereda la clase
+    esta función no puede ser invocada.
+
+    pep-0008:
+        _single_leading_underscore: 
+        weak “internal use” indicator. E.g. from M import * does not import objects whose names start with an underscore.
+    '''
+    
     def get_location_address(self, location_name):
         location_address = {}
         match_query = {
