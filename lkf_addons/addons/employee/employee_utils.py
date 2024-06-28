fbced6e21315f530ad77a7e0d70231abbe92597b
KarinaMoreno <dianakarina.moreno21@gmail.com>
2024-06-28 11:50:44 -0600

    Agregué documentación en el archivo employee_utils.py del módulo employee

--- lkf_addons/addons/employee/employee_utils.py
+++ lkf_addons/addons/employee/employee_utils.py
@@ -1,10 +1,47 @@
 # -*- coding: utf-8 -*-
+### Linkaform Modules / Archivo de Módulo ###
+'''
+Este archivo proporciona las funcionalidades modulares de LinkaForm. Con estas funcionalidades, 
+podrás utilizar la plataforma LinkaForm de manera modular, como un Backend as a Service (BaaS).
+
+Licencia
+Este código está licenciado bajo la licencia GPL3 (https://www.gnu.org/licenses/gpl-3.0.html).
+
+Propósito
+El propósito de este archivo es ser auto documentable y adaptable, facilitando la reutilización 
+de gran parte del código en otros módulos simplemente copiando y pegando las secciones necesarias.
+
+Instrucciones
+1. Al copiar secciones de código, asegúrate de incluir la documentación correspondiente.
+2. Al crear un nuevo archivo o módulo, copia las instrucciones y las generales aplicables a cada archivo.
+3. Puedes basarte en la carpeta `_templates` o sus archivos para crear nuevos módulos.
+'''
+
+### Archivo de Modulo ###
+'''
+Este archivo define las funciones generales del módulo. Por conveniencia, se nombra `app.py`. 
+
+Si tienes más de una aplicación, puedes:
+    a. Crear una carpeta llamada `app`.
+    b. Guardar los archivos a nivel raíz.
+    c. Nombrar los archivos por conveniencia o estándar: `app_utils.py`, `utils.py`, `xxx_utils.py`.
+'''
+
+# Importaciones necesarias
 import sys, simplejson
 
 from linkaform_api import settings
 from linkaform_api import base
 from lkf_addons.addons.base.base_util import Base
 
+### Objeto o Clase de Módulo ###
+'''
+Cada módulo puede tener múltiples objetos, configurados en clases.
+Estos objetos deben heredar de `base.LKF_Base` y de cualquier módulo dependiente necesario.
+Al utilizar `super()` en el método `__init__()`, heredamos las variables de configuración de la clase.
+
+Además, se pueden heredar funciones de cualquier clase antecesora usando el método `super()`.
+'''
 
 class Employee(Base, base.LKF_Base):
 
@@ -13,11 +50,25 @@ class Employee(Base, base.LKF_Base):
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
         self.name =  __class__.__name__
         self.settings = settings
-        # forms
+
+        #--Variables 
+        ### Forms ###
+        '''
+        Use `self.FORM_NAME = self.lkm.form_id('form_name',id)` ---> Aquí deberás guardar los `ID` de los formularios. 
+        Para ello deberás llamar el método `lkm.form_id` del objeto `lkm` (linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos.
+        '''
         self.CONF_AREA_EMPLEADOS = self.lkm.form_id('configuracion_areas_y_empleados', 'id')
         self.CONF_DEPARTAMENTOS_PUESTOS = self.lkm.form_id('configuracion_de_departamentos_y_puestos', 'id')
         self.EMPLEADOS = self.lkm.form_id('empleados','id')
-        # catalgos
+
+        #--Variables 
+        ### Catálogos ###
+        '''
+        Use `self.CATALOG_NAME = self.lkm.catalog_id('catalog_name',id)` ---> Aquí deberás guardar los `ID` de los catálogos. 
+        Para ello deberás llamar el método `lkm.catalog_id` del objeto `lkm`(linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos).
+        '''
         self.EMPLOYEE = self.lkm.catalog_id('employee')
         self.EMPLOYEE_ID = self.EMPLOYEE.get('id')
         self.EMPLOYEE_OBJ_ID = self.EMPLOYEE.get('obj_id')
@@ -38,6 +89,12 @@ class Employee(Base, base.LKF_Base):
         self.EMPLEADOS_JEFES_DIRECTOS_ID = self.EMPLEADOS_JEFES_DIRECTOS.get('id')
         self.EMPLEADOS_JEFES_DIRECTOS_OBJ_ID = self.EMPLEADOS_JEFES_DIRECTOS.get('obj_id')
 
+        ## Module Fields ##
+        ''' 
+        self.mf : Estos son los campos que deseas mantener solo dentro de este modulo.
+        Asegúrese de utilizar `llave` y el `id` del campo ej.
+        'nombre_campo': "1f2h3j4j5d6f7h8j9j1a",
+        '''
         self.employee_fields = {
             'worker_name':'62c5ff407febce07043024dd',
             'worker_department':'663bc4ed8a6b120eab4d7f1e',
@@ -57,11 +114,33 @@ class Employee(Base, base.LKF_Base):
             'genero':'663bcbe2274189281359eb75',
             'status_en_empresa':'663bcbe2274189281359eb77',
             'cat_timezone':self.f['cat_timezone'],
-                }
+        }
+
+        ### Fields ###
+        '''
+        `self.f`: En esta variable "fields", se almacenan todos los campos de todos los módulos heredados.
+        El orden de reemplazo se ve afectado por el orden en que se hereda cada módulo. El orden que se otorga, es considerando
+        que la variable se iguala en la base, y se va armando en tren de dependencias ej.
+
+            Class A:
+            Class B(A):
+            Class C(B):
+            Class D(C):
+
+            x_obj = D()
+            el orden de herencia será, primero carga A > B > C > D.
+        '''
 
         self.f.update(self.employee_fields)
 
+    '''
+    funciones internas: son funciones que solo se pueden mandar llamar dentro de este archivo. Si se hereda la clase
+    esta función no puede ser invocada.
 
+    pep-0008:
+        _single_leading_underscore: 
+        weak “internal use” indicator. E.g. from M import * does not import objects whose names start with an underscore.
+    '''
 
     def _get_match_q(self, field_id, value):
         if type(value) == list:
