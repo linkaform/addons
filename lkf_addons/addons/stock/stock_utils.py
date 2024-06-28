6ace0d00d4a435c66830c25806a31862d1173ef9
KarinaMoreno <dianakarina.moreno21@gmail.com>
2024-06-28 11:59:19 -0600

    Agregué documentación en el archivo stock_utils.py del módulo stock

--- lkf_addons/addons/stock/stock_utils.py
+++ lkf_addons/addons/stock/stock_utils.py
@@ -1,15 +1,50 @@
 # -*- coding: utf-8 -*-
+### Linkaform Modules / Archivo de Módulo ###
+'''
+Este archivo proporciona las funcionalidades modulares de LinkaForm. Con estas funcionalidades, 
+podrás utilizar la plataforma LinkaForm de manera modular, como un Backend as a Service (BaaS).
 
+Licencia
+Este código está licenciado bajo la licencia GPL3 (https://www.gnu.org/licenses/gpl-3.0.html).
+
+Propósito
+El propósito de este archivo es ser auto documentable y adaptable, facilitando la reutilización 
+de gran parte del código en otros módulos simplemente copiando y pegando las secciones necesarias.
+
+Instrucciones
+1. Al copiar secciones de código, asegúrate de incluir la documentación correspondiente.
+2. Al crear un nuevo archivo o módulo, copia las instrucciones y las generales aplicables a cada archivo.
+3. Puedes basarte en la carpeta `_templates` o sus archivos para crear nuevos módulos.
+'''
+
+### Archivo de Modulo ###
+'''
+Este archivo define las funciones generales del módulo. Por conveniencia, se nombra `app.py`. 
+
+Si tienes más de una aplicación, puedes:
+    a. Crear una carpeta llamada `app`.
+    b. Guardar los archivos a nivel raíz.
+    c. Nombrar los archivos por conveniencia o estándar: `app_utils.py`, `utils.py`, `xxx_utils.py`.
+'''
+
+# Importaciones necesarias
 from datetime import timedelta, datetime
 import math, simplejson, time
 from copy import deepcopy
-
 
 from linkaform_api import base
 
 from lkf_addons.addons.employee.employee_utils import Employee
 from lkf_addons.addons.product.product_utils import Product, Warehouse
 
+### Objeto o Clase de Módulo ###
+'''
+Cada módulo puede tener múltiples objetos, configurados en clases.
+Estos objetos deben heredar de `base.LKF_Base` y de cualquier módulo dependiente necesario.
+Al utilizar `super()` en el método `__init__()`, heredamos las variables de configuración de la clase.
+
+Además, se pueden heredar funciones de cualquier clase antecesora usando el método `super()`.
+'''
 
 class Stock(Employee, Warehouse, Product, base.LKF_Base):
 
@@ -19,6 +54,13 @@ class Stock(Employee, Warehouse, Product, base.LKF_Base):
         self.name =  __class__.__name__
         self.settings = settings
 
+        #--Variables 
+        ### Catálogos ###
+        '''
+        Use `self.CATALOG_NAME = self.lkm.catalog_id('catalog_name',id)` ---> Aquí deberás guardar los `ID` de los catálogos. 
+        Para ello deberás llamar el método `lkm.catalog_id` del objeto `lkm`(linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos).
+        '''
         print('lokf', self.lkm)
         self.STOCK_INVENTORY = self.lkm.catalog_id('stock_inventory')
         self.STOCK_INVENTORY_ID = self.STOCK_INVENTORY.get('id')
@@ -33,6 +75,13 @@ class Stock(Employee, Warehouse, Product, base.LKF_Base):
         self.CATALOG_INVENTORY_OBJ_ID = self.CATALOG_INVENTORY.get('obj_id')
         ########################
 
+        #--Variables 
+        ### Forms ###
+        '''
+        Use `self.FORM_NAME = self.lkm.form_id('form_name',id)` ---> Aquí deberás guardar los `ID` de los formularios. 
+        Para ello deberás llamar el método `lkm.form_id` del objeto `lkm` (linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos.
+        '''
         self.FORM_INVENTORY_ID = self.lkm.form_id('stock_inventory','id')
         self.ADJUIST_FORM_ID = self.lkm.form_id('stock_inventory_adjustment','id')
         # self.STOCK_MOVE_FORM_ID = self.lkm.form_id('stock_inventory_move','id')
@@ -55,8 +104,6 @@ class Stock(Employee, Warehouse, Product, base.LKF_Base):
             self.FORM_INVENTORY_ID: self.CATALOG_INVENTORY_ID, #Inventory Flow (greenHouse)
             }
 
-
-
         self.container_per_rack = {
                 'Baby Jar': 38,
                 'baby_jar': 38,
@@ -75,6 +122,21 @@ class Stock(Employee, Warehouse, Product, base.LKF_Base):
         # self.f stands for field 
         # self.f is use to realte a human name to a objectId where the ObjectId
         # ex: self.f = {'key':'ObjectId'}
+
+        ### Fields ###
+        '''
+        `self.f`: En esta variable "fields", se almacenan todos los campos de todos los módulos heredados.
+        El orden de reemplazo se ve afectado por el orden en que se hereda cada módulo. El orden que se otorga, es considerando
+        que la variable se iguala en la base, y se va armando en tren de dependencias ej.
+
+            Class A:
+            Class B(A):
+            Class C(B):
+            Class D(C):
+
+            x_obj = D()
+            el orden de herencia será, primero carga A > B > C > D.
+        '''
 
         self.f.update({
             'actual_eaches_on_hand':'620ad6247a217dbcb888d172',
@@ -213,6 +275,15 @@ class Stock(Employee, Warehouse, Product, base.LKF_Base):
             'worker_obj_id':'62c5ff243c63280985580087',
         })
 
+    '''
+    funciones internas: son funciones que solo se pueden mandar llamar dentro de este archivo. Si se hereda la clase
+    esta función no puede ser invocada.
+
+    pep-0008:
+        _single_leading_underscore: 
+        weak “internal use” indicator. E.g. from M import * does not import objects whose names start with an underscore.
+    '''
+    
     def add_dicts(self, dict1, dict2):
         for key in dict1:
             dict1[key] += dict2.get(key,0)
