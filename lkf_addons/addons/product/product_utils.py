5a508b91d5484cfd4f7679f64fb9f9a55ef39b9d
KarinaMoreno <dianakarina.moreno21@gmail.com>
2024-06-28 11:57:08 -0600

    Agregué documentación en el archivo product_utils.py del módulo product

--- lkf_addons/addons/product/product_utils.py
+++ lkf_addons/addons/product/product_utils.py
@@ -1,8 +1,44 @@
 # -*- coding: utf-8 -*-
+### Linkaform Modules / Archivo de Módulo ###
+'''
+Este archivo proporciona las funcionalidades modulares de LinkaForm. Con estas funcionalidades, 
+podrás utilizar la plataforma LinkaForm de manera modular, como un Backend as a Service (BaaS).
 
+Licencia
+Este código está licenciado bajo la licencia GPL3 (https://www.gnu.org/licenses/gpl-3.0.html).
+
+Propósito
+El propósito de este archivo es ser auto documentable y adaptable, facilitando la reutilización 
+de gran parte del código en otros módulos simplemente copiando y pegando las secciones necesarias.
+
+Instrucciones
+1. Al copiar secciones de código, asegúrate de incluir la documentación correspondiente.
+2. Al crear un nuevo archivo o módulo, copia las instrucciones y las generales aplicables a cada archivo.
+3. Puedes basarte en la carpeta `_templates` o sus archivos para crear nuevos módulos.
+'''
+
+### Archivo de Modulo ###
+'''
+Este archivo define las funciones generales del módulo. Por conveniencia, se nombra `app.py`. 
+
+Si tienes más de una aplicación, puedes:
+    a. Crear una carpeta llamada `app`.
+    b. Guardar los archivos a nivel raíz.
+    c. Nombrar los archivos por conveniencia o estándar: `app_utils.py`, `utils.py`, `xxx_utils.py`.
+'''
+
+# Importaciones necesarias
 from linkaform_api import base
 from lkf_addons.addons.base.base_util import Base
 
+### Objeto o Clase de Módulo ###
+'''
+Cada módulo puede tener múltiples objetos, configurados en clases.
+Estos objetos deben heredar de `base.LKF_Base` y de cualquier módulo dependiente necesario.
+Al utilizar `super()` en el método `__init__()`, heredamos las variables de configuración de la clase.
+
+Además, se pueden heredar funciones de cualquier clase antecesora usando el método `super()`.
+'''
 
 class Product(Base, base.LKF_Base):
 
@@ -12,6 +48,21 @@ class Product(Base, base.LKF_Base):
         self.name =  __class__.__name__
         self.settings = settings
 
+        #--Variables 
+        ### Forms ###
+        '''
+        Use `self.FORM_NAME = self.lkm.form_id('form_name',id)` ---> Aquí deberás guardar los `ID` de los formularios. 
+        Para ello deberás llamar el método `lkm.form_id` del objeto `lkm` (linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos.
+        '''
+        
+        #--Variables 
+        ### Catálogos ###
+        '''
+        Use `self.CATALOG_NAME = self.lkm.catalog_id('catalog_name',id)` ---> Aquí deberás guardar los `ID` de los catálogos. 
+        Para ello deberás llamar el método `lkm.catalog_id` del objeto `lkm`(linkaform modules, por sus siglas).
+        En `lkm` están todas las funciones generales de módulos).
+        '''
         self.PRODUCT = self.lkm.catalog_id('product_catalog')
         self.PRODUCT_ID = self.PRODUCT.get('id')
         self.PRODUCT_OBJ_ID = self.PRODUCT.get('obj_id')
@@ -34,6 +85,21 @@ class Product(Base, base.LKF_Base):
             self.PRODUCT_RECIPE_ID = self.SKU.get('id')
             self.PRODUCT_RECIPE_OBJ_ID = self.SKU.get('obj_id')
 
+        ### Fields ###
+        '''
+        Use `self.f`: En esta variable "fields", se almacenan todos los campos de todos los módulos heredados.
+        El orden de reemplazo se ve afectado por el orden en que se hereda cada módulo. El orden que se otorga, es considerando
+        que la variable se iguala en la base, y se va armando en tren de dependencias ej.
+
+            Class A:
+            Class B(A):
+            Class C(B):
+            Class D(C):
+
+            x_obj = D()
+            el orden de herencia será, primero carga A > B > C > D.
+        '''
+
         self.f.update( {
             'product_code':'61ef32bcdf0ec2ba73dec33d',
             'product_name':'61ef32bcdf0ec2ba73dec33e',
@@ -52,6 +118,15 @@ class Product(Base, base.LKF_Base):
             'sku_source':'6205f73281bb36a6f157335a',
             }
             )
+
+    '''
+    funciones internas: son funciones que solo se pueden mandar llamar dentro de este archivo. Si se hereda la clase
+    esta función no puede ser invocada.
+
+    pep-0008:
+        _single_leading_underscore: 
+        weak “internal use” indicator. E.g. from M import * does not import objects whose names start with an underscore.
+    '''
 
     def una_funcion_product(self):
         return True
@@ -77,11 +152,8 @@ class Product(Base, base.LKF_Base):
                 return rec
             product_field = rec.get(self.f[pfield])
         return product_field
-
-
-
+    
 class Warehouse(Base ,base.LKF_Base):
-
 
     def __init__(self, settings, folio_solicitud=None, sys_argv=None, use_api=False):
         # base.LKF_Base.__init__(self, settings, sys_argv=sys_argv)
