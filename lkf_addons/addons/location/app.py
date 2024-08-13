b44804beb860eca208cbed0fb9b9f6b82a1eadf5
Linkaform Develop Team <backend@linkaform.com>
2024-08-12 23:45:17 -0600

    se cambia el nombre de archivo

---
+++ lkf_addons/addons/location/app.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+
+from linkaform_api import base
+from lkf_addons.addons.base.base_util import Base
+
+
+class Location(Base, base.LKF_Base):
+
+    def __init__(self, settings, folio_solicitud=None, sys_argv=None, use_api=False):
+        super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
+        #use self.lkm.catalog_id() to get catalog id
+        # forms
+        self.AREAS_DE_LAS_UBICACIONES = self.lkm.form_id('areas_de_las_ubicaciones', 'id')
+        self.UBICACIONES = self.lkm.form_id('ubicaciones', 'id')
+        # catalgos
+        self.UBICACIONES_CAT = self.lkm.catalog_id('ubicaciones')
+        self.UBICACIONES_CAT_ID = self.UBICACIONES_CAT.get('id')
+        self.UBICACIONES_CAT_OBJ_ID = self.UBICACIONES_CAT.get('obj_id')
+
+        self.AREAS_DE_LAS_UBICACIONES_CAT = self.lkm.catalog_id('areas_de_las_ubicaciones')
+        self.AREAS_DE_LAS_UBICACIONES_CAT_ID = self.AREAS_DE_LAS_UBICACIONES_CAT.get('id')
+        self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID = self.AREAS_DE_LAS_UBICACIONES_CAT.get('obj_id')
+
+        self.AREAS_DE_LAS_UBICACIONES_SALIDA = self.lkm.catalog_id('areas_de_las_ubicaciones_salidas')
+        self.AREAS_DE_LAS_UBICACIONES_SALIDA_ID = self.AREAS_DE_LAS_UBICACIONES_SALIDA.get('id')
+        self.AREAS_DE_LAS_UBICACIONES_SALIDA_OBJ_ID = self.AREAS_DE_LAS_UBICACIONES_SALIDA.get('obj_id')
+
+        self.TIPO_AREA = self.lkm.catalog_id('tipo_de_areas')
+        self.TIPO_AREA_ID = self.TIPO_AREA.get('id')
+        self.TIPO_AREA_OBJ_ID = self.TIPO_AREA.get('obj_id')
+
+        self.f.update( {
+            'location':'663e5c57f5b8a7ce8211ed0b',
+            'area':'663e5d44f5b8a7ce8211ed0f',
+            'area_state':'663e5e4bf5b8a7ce8211ed14',
+            'area_status':'663e5e4bf5b8a7ce8211ed15',
+        }
+        )
+
+    def get_location_address(self, location_name):
+        location_address = {}
+        match_query = {
+            "deleted_at":{"$exists":False},
+            "form_id": self.UBICACIONES,
+            f"answers.{self.f['location']}":location_name
+            }
+        query = [
+            {'$match': match_query },
+            {'$project':
+                {'_id': 1,
+                    'folio': "$folio",
+                    'location': f"$answers.{self.f['location']}",
+                    'address_name': f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address_name']}",
+                    'address': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address']}"},
+                    'address2': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address2']}"},
+                    'address_type': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address_type']}"},
+                    'address_geolocation': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address_geolocation']}"},
+                    'state': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['state']}"},
+                    'city': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['city']}"},
+                    'zip_code': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['zip_code']}"},
+                    'country': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['country']}"},
+                    'phone': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['phone']}"},
+                    'email': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['email']}"},
+                    }
+            }
+            ]
+        res = self.cr.aggregate(query)
+        for x in res:
+            location_address = x
+        return location_address
+
+    def get_area_address(self, location_name, area_name):
+        match_query = {
+            "deleted_at":{"$exists":False},
+            "form_id": self.AREAS_DE_LAS_UBICACIONES,
+            f"answers.{self.UBICACIONES_CAT_OBJ_ID}.{self.f['location']}":location_name,
+            f"answers.{self.f['area']}":area_name
+            }
+        query = [
+            {'$match': match_query },
+            {'$project':
+                {'_id': 1,
+                    'folio': "$folio",
+                    'area': f"$answers.{self.f['area']}",
+                    'location': f"$answers.{self.UBICACIONES_CAT_OBJ_ID}.{self.f['location']}",
+                    'address_name': f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address_name']}",
+                    'address': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address']}"},
+                    'address2': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address2']}"},
+                    'address_type': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address_type']}"},
+                    'address_geolocation': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['address_geolocation']}"},
+                    'state': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['state']}"},
+                    'city': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['city']}"},
+                    'zip_code': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['zip_code']}"},
+                    'country': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['country']}"},
+                    'phone': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['phone']}"},
+                    'email': {'$first':f"$answers.{self.CONTACTO_CAT_OBJ_ID}.{self.f['email']}"},
+                    }
+            }
+            ]
+        import simplejson
+        # print('query=', simplejson.dumps(query, indent=4))
+        res = self.cr.aggregate(query)
+        area_address = {}
+        for x in res:
+            area_address = x
+        if not area_address:
+            area_address = self.get_location_address(location_name)
+        return area_address
+
+
+
+    def get_area_status(self, location, area, state='activa'):
+        match_query = {
+            "deleted_at":{"$exists":False},
+            "form_id": self.AREAS_DE_LAS_UBICACIONES,
+            f"answers.{self.UBICACIONES_CAT_OBJ_ID}.{self.f['location']}":location,
+            f"answers.{self.f['area']}":area,
+            f"answers.{self.f['area_state']}":state,
+        }
+        res = self.format_cr(self.cr.find(match_query, {f"answers.{self.f['area_status']}":1}), get_one=True)
+        ans = res.get('answers',{})
+        res = ans.get(self.f['area_status'], 'No Configurada')
+        return res.title()
