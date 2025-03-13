57a002f50460bdf81d9aa15aace61fc9e4bbbfd1
paco soto <francisotomtz@gmail.com>
2025-03-12 22:24:34 -0600

    se crea base del modulo de mantenimiento en addons

---
+++ lkf_addons/addons/mantenimiento/items/catalogs/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+from lkf_addons.items.catalog_resource import CatalogResource
+
+install_order = [
+    'ciudades_por_region',
+    'clientes',
+    'instrumentos',
+    'inventario_de_equipos'
+]
