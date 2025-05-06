b5afe8599a5dcdf7acbc995d03eeee732169e02f
paco soto <francisotomtz@gmail.com>
2025-05-06 08:44:19 -0600

    fix: Se actualiza el orden de instalacion de los catalogos del modulo Product

--- lkf_addons/addons/product/items/catalogs/__init__.py
+++ lkf_addons/addons/product/items/catalogs/__init__.py
@@ -1,4 +1,4 @@
 # coding: utf-8
 from lkf_addons.items.catalog_resource import CatalogResource
 
-install_order = ['product_catalog','tipo_de_lista_de_precio','services']
+install_order = ['product_catalog','tipo_de_lista_de_precio','services','warehouse','warehouse_destination']
