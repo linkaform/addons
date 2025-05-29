668452dad00c3a560291bba7998bd0d2171b1e71
paco soto <francisotomtz@gmail.com>
2025-05-29 15:49:32 -0600

    refactor: Se agregar total de entradas en el servicio que obtiene el pase de entrada en Accesos

--- lkf_addons/addons/accesos/app.py
+++ lkf_addons/addons/accesos/app.py
@@ -4793,6 +4793,7 @@ class Accesos(Employee, Location, Vehiculo, base.LKF_Base):
             access_pass['limitado_a_dias']= access_pass.get('limitado_a_dias','')
             access_pass['limitado_a_acceso']= access_pass.get('limite_de_acceso','')
             access_pass['config_dia_de_acceso']=access_pass.get('config_dia_de_acceso',"").replace("_", " ")
+            access_pass['total_entradas'] = self.get_count_ingresos(qr_code)
             if access_pass.get('grupo_areas_acceso'):
                 for area in access_pass['grupo_areas_acceso']:
                     area['status'] = self.get_area_status(access_pass['ubicacion'], area['nombre_area'])
