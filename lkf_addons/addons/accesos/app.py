244f6604c9d811f32bde924255977d7186255093
pacosotto <francisotomtz@gmail.com>
2026-07-29 17:35:36 -0600

    fix: Se incluyen campos obligatorios de incidencias

--- lkf_addons/addons/accesos/app.py
+++ lkf_addons/addons/accesos/app.py
@@ -10472,6 +10472,8 @@ class Accesos(OcrMixin, AccesosModel):
                 self.incidence_fields['area_incidencia']: incidencia.get('area', ''),
             },
             self.incidence_fields['estatus']: 'abierto',
+            self.incidence_fields['prioridad_incidencia']: incidencia.get('prioridad_incidencia', 'baja'),
+            self.incidence_fields['notificacion_incidencia']: incidencia.get('notificacion_incidencia', 'no'),
             self.incidence_fields['fecha_hora_incidencia']: incidencia.get('fecha_incidencia', ''),
             self.incidence_fields['comentario_incidencia']: incidencia.get('comentario_incidente_bitacora', ''),
             self.incidence_fields['evidencia_incidencia']: [i for i in incidencia.get('incidente_evidencia', []) if i.get('file_url', '')],
