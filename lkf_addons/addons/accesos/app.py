e2076e70c8ff7f2afcfa08b3df4ffb3942cf5c15
Paco Soto <francisotomtz@gmail.com>
2026-08-28 16:01:50 -0600

    fix: Se valida respuesta de get_pdf_multi para no retornar un ObjId

--- lkf_addons/addons/accesos/app.py
+++ lkf_addons/addons/accesos/app.py
@@ -6538,7 +6538,10 @@ class Accesos(OcrMixin, AccesosModel):
         templates = self.lkf_api.get_pdf_templates(self.PASE_ENTRADA) or []
         template_id = next((t.get('id') for t in templates if t.get('_type') == 'multiple-records'), None)
         records_uri = ['/api/infosync/form_answer/{}/'.format(rid) for rid in record_ids]
-        return self.lkf_api.get_pdf_record(records_uri, template_id=template_id, name_pdf=name_pdf)
+        res = self.lkf_api.get_pdf_record(records_uri, template_id=template_id, name_pdf=name_pdf)
+        if res:
+            res['_id'] = str(res.get('_id', ''))
+        return res
 
     def get_paquetes(self, location= "", area="", status="", dateFrom="", dateTo="", filterDate=""):
         match_query = {
