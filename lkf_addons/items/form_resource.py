ea9af5460faf1ea699dab82ca828456d32e0df05
Jose Patricio Villarreal <josepato@hotmail.com>
2026-08-30 23:26:33 -0600

    fix: fomra isn fecha

--- lkf_addons/items/form_resource.py
+++ lkf_addons/items/form_resource.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 import simplejson
+import time
 
 from linkaform_api import utils, lkf_models
+from linkaform_api.network import HTTP_TIMING
 
 from lkf_addons import items 
 
@@ -56,6 +58,7 @@ class FormResource(items.Items):
 
         # install_order = ['green_house_inventory_move']
         for form_name in install_order:
+            form_started_at = time.time() if HTTP_TIMING else None
             detail = instalable_forms[form_name]
             if detail.get('path'):
                 this_path = '{}/{}'.format(self.path, detail['path'])
@@ -97,6 +100,11 @@ class FormResource(items.Items):
                 print('Status Code 400:', res)
                 error = res.get('json',{}).get('error','Please try again!!!')
                 raise self.LKFException(f'Error installing form: {form_name}. Error msg {error}')
+            if form_started_at is not None:
+                # Total por forma (plantilla + form + workflows + rules), para separar
+                # el tiempo del back del tiempo local de render/parseo del XML.
+                print('[form] {:>7.2f}s  {} ({})'.format(
+                    time.time() - form_started_at, form_name, res.get('status', 'sin cambios')))
         return response
  
     def get_form_modules(self, all_items, parent_path=None):
