b549b59de31069e5d97582127034028fa75ac0fd
Paco Soto <francisotomtz@gmail.com>
2026-09-16 14:24:24 -0600

    feat: Se gestiona orden de instalacion de catalogs en modulo activo fijo

--- lkf_addons/addons/activo_fijo/items/catalogs/__init__.py
+++ lkf_addons/addons/activo_fijo/items/catalogs/__init__.py
@@ -1,5 +1,5 @@
 # coding: utf-8
 from lkf_addons.items.catalog_resource import CatalogResource
 
-install_order = ['tipo_de_equipos', 'tipos_de_vehiculo','marca', 'modelo']
+install_order = ['categorias_de_objetos', 'tipos_de_equipos', 'tipos_de_vehiculo','marca', 'modelo']
 
